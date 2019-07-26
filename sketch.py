00550cec23c481791ac3ddfeedd0a6a506cad3d5
GongBiao <github@gongbiao.cc>
2019-07-26 18:31:44 +0800

    cj

--- sketch.py
+++ sketch.py
@@ -1,19 +1,96 @@
 import os
+from data import guipang
+import torch.utils.data as data
+import copy
+import os
+import sys
+import json
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+# from config import get_train_config
+from data import guipang
+from data import qiafan
+# from models.MVHNet import *
+# from utils import append_feature, calculate_map
+import time
+import shutil
+from tensorboardX import SummaryWriter
+import pdb
 dataset_guipang= '/repository/gong/qiafan/guipangdata/'
-
+# config
+cfg = {
+    'lr': 0.001,
+    'momentum': 0.9,
+    'weight_decay': 0.0005,
+    'batch_size': 4,
+    'max_epoch': 250,
+    'checkpoint': 100,
+    'milestones': [60, 100],
+    'gamma': 0.1,
+    'dataset_guipang': '/repository/gong/qiafan/guipangdata/',
+    'dataset_qiafan': '/repository/gong/qiafan/dataset/',
+    'cuda_devices': '6,7',
+    'ckpt_root': '/repository/gong/qiafan/'
+}
 # print(os.path.join(dataset_guipang,'train')
 # for filename in os.listdir(os.path.join(dataset_guipang,'train')):
 #     if os.path.splitext(filename)[1]=='.jpg':
 #         print(filename)
-images=[]
-annotations=[]
+# images=[]
+# annotations=[]
 
-for filename in os.listdir(os.path.join(dataset_guipang,'train')):
-    if os.path.splitext(filename)[1]=='.jpg':
-        images.append(filename)
-        annotations.append(os.path.splitext(filename)[0]+'.xml')
+# for filename in os.listdir(os.path.join(dataset_guipang,'train')):
+#     if os.path.splitext(filename)[1]=='.jpg':
+#         images.append(filename)
+#         annotations.append(os.path.splitext(filename)[0]+'.xml')
 
     # elif os.path.splitext(filename)[1]=='.xml':
     #     annotations.append(filename)
-print(images)
-print(annotations)
+data_set = {
+    x: guipang(cfg=cfg['dataset_guipang'], part=x) for x in ['train', 'val']
+}
+# data_set = {
+#     x: dataset(cfg=cfg['dataset_qiafan'], part=x) for x in ['train', 'val']
+# }
+data_loader = {
+    x: data.DataLoader(data_set[x], batch_size=cfg['batch_size'],
+                        num_workers=4, shuffle=True, pin_memory=False)
+    for x in ['train', 'val']
+}
+# a=[1,2]
+# print(a)
+# pdb.set_trace()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = cfg['cuda_devices']
+
+
+model = torchvision.models.detection.maskrcnn_resnet50_fpn()
+model.cuda()
+model = nn.DataParallel(model)
+model.train()
+
+for i, (images, annotations) in enumerate(data_loader['train']):
+    # print(images)
+    # # print(annotations['annotation']['object']['name'])
+    # # annotations=torch.FloatTensor(list(map(int,annotations['annotation']['object']['name'])))
+    # # print(annotations['annotation']['object']['bndbox']['xmin'])
+    # print(annotations['annotation']['object']['bndbox'])
+
+    # print(torch.tensor([list(map(int,annotations['annotation']['object']['bndbox']['xmin'])),
+    #     list(map(int,annotations['annotation']['object']['bndbox']['ymin']))]).t())
+    # pdb.set_trace()
+    images = torch.cuda.FloatTensor(images.cuda())
+    boxes = torch.cuda.FloatTensor([list(map(int,annotations['annotation']['object']['bndbox']['xmin'])),
+                                    list(map(int,annotations['annotation']['object']['bndbox']['ymin'])),
+                                    list(map(int,annotations['annotation']['object']['bndbox']['xmax'])),
+                                    list(map(int,annotations['annotation']['object']['bndbox']['ymax']))]).t()
+    labels = torch.cuda.LongTensor(
+        list(map(int, annotations['annotation']['object']['name'])))
+
+    with torch.set_grad_enabled(phrase == 'train'):
+        outputs = model(images,boxes=boxes,labels=labels)
+        print(outputs)
+        pdb.set_trace()
--- train_qiafan.py
+++ train_qiafan.py
@@ -7,14 +7,14 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import torchvision
-# from config import get_train_config
 from data import guipang
 from data import qiafan
-# from models.MVHNet import *
+import pdb
 # from utils import append_feature, calculate_map
 import time
 import shutil
 from tensorboardX import SummaryWriter
+
 
 def train_guipang(model, criterion, optimizer, scheduler, cfg):
 
@@ -40,51 +40,58 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
 
                 optimizer.zero_grad()
 
-                images = torch.cuda.FloatTensor(centers.cuda())
-                corners = torch.cuda.FloatTensor(corners.cuda())
-                normals = torch.cuda.FloatTensor(normals.cuda())
-                neighbor_index = torch.cuda.LongTensor(neighbor_index.cuda())
-                targets = torch.cuda.LongTensor(targets.cuda())
+                images = torch.cuda.FloatTensor(images.cuda())
+                boxes = torch.cuda.FloatTensor([list(map(int,annotations['annotation']['object']['bndbox']['xmin'])),
+                                                list(map(int,annotations['annotation']['object']['bndbox']['ymin'])),
+                                                list(map(int,annotations['annotation']['object']['bndbox']['xmax'])),
+                                                list(map(int,annotations['annotation']['object']['bndbox']['ymax']))]).t()
+                labels = torch.cuda.LongTensor(
+                    list(map(int, annotations['annotation']['object']['name'])))
 
                 with torch.set_grad_enabled(phrase == 'train'):
-                    fea_hash_mesh, fea_hash_sensib_mesh, outputs = model(
-                        (centers, corners, normals, neighbor_index))
-                    _, preds = torch.max(outputs, 1)
-                    # loss = criterion['crossentropyloss'](
-                    #     outputs, targets)+criterion[cfg['maskloss']](fea_hash_sensib_mesh, fea_hash_mesh)
-                    lossa = criterion['crossentropyloss'](outputs, targets)
-                    lossb = criterion[cfg['maskloss']](
-                        fea_hash_sensib_mesh, fea_hash_mesh)
-                    # loss=lossa+lossa*lossb
-                    loss = lossa+lossb
-                    if phrase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                    running_loss += loss.item() * centers.size(0)
-                    running_corrects += torch.sum(preds == targets.data)
+                    outputs = model(images,boxes=boxes,labels=labels)
+                    print(outputs)
+                    pdb.set_trace()
+                    
+                    
+                    
+                    
+            #         _, preds = torch.max(outputs, 1)
+            #         # loss = criterion['crossentropyloss'](
+            #         #     outputs, targets)+criterion[cfg['maskloss']](fea_hash_sensib_mesh, fea_hash_mesh)
+            #         lossa = criterion['crossentropyloss'](outputs, targets)
+            #         lossb = criterion[cfg['maskloss']](
+            #             fea_hash_sensib_mesh, fea_hash_mesh)
+            #         # loss=lossa+lossa*lossb
+            #         loss = lossa+lossb
+            #         if phrase == 'train':
+            #             loss.backward()
+            #             optimizer.step()
+            #         running_loss += loss.item() * centers.size(0)
+            #         running_corrects += torch.sum(preds == targets.data)
 
-                if phrase == 'train':
-                    writer.add_scalar('mesh/realtime/globalLoss',
-                                      loss.item(), i+nowepochiter)
+            #     if phrase == 'train':
+            #         writer.add_scalar('mesh/realtime/globalLoss',
+            #                           loss.item(), i+nowepochiter)
 
-            epoch_loss = running_loss / len(data_set[phrase])
-            epoch_acc = running_corrects.double() / len(data_set[phrase])
+            # epoch_loss = running_loss / len(data_set[phrase])
+            # epoch_acc = running_corrects.double() / len(data_set[phrase])
 
-            if phrase == 'train':
-                writer.add_scalar('mesh/epoch/globalLoss', epoch_loss, epoch)
-                writer.add_scalar('mesh/epoch/Acc', epoch_acc, epoch)
+            # if phrase == 'train':
+            #     writer.add_scalar('mesh/epoch/globalLoss', epoch_loss, epoch)
+            #     writer.add_scalar('mesh/epoch/Acc', epoch_acc, epoch)
 
-            if phrase == 'val':
-                if epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.module.state_dict())
-                    torch.save(best_model_wts, the_ckpt_root+'best.pkl')
-                if epoch % cfg['checkpoint'] == 0:
-                    torch.save(copy.deepcopy(model.module.state_dict()),
-                               the_ckpt_root+'{}.pkl'.format(epoch))
-                writer.add_scalar(
-                    'mesh/epochtest/globalLoss', epoch_loss, epoch)
-                writer.add_scalar('mesh/epochtest/Acc', epoch_acc, epoch)
+            # if phrase == 'val':
+            #     if epoch_acc > best_acc:
+            #         best_acc = epoch_acc
+            #         best_model_wts = copy.deepcopy(model.module.state_dict())
+            #         torch.save(best_model_wts, the_ckpt_root+'best.pkl')
+            #     if epoch % cfg['checkpoint'] == 0:
+            #         torch.save(copy.deepcopy(model.module.state_dict()),
+            #                    the_ckpt_root+'{}.pkl'.format(epoch))
+            #     writer.add_scalar(
+            #         'mesh/epochtest/globalLoss', epoch_loss, epoch)
+            #     writer.add_scalar('mesh/epochtest/Acc', epoch_acc, epoch)
 
     return best_model_wts
 
@@ -97,7 +104,7 @@ if __name__ == '__main__':
         'lr': 0.001,
         'momentum': 0.9,
         'weight_decay': 0.0005,
-        'batch_size': 60,
+        'batch_size': 4,
         'max_epoch': 250,
         'checkpoint': 100,
         'milestones': [60, 100],
@@ -123,16 +130,15 @@ if __name__ == '__main__':
     #     x: dataset(cfg=cfg['dataset_qiafan'], part=x) for x in ['train', 'val']
     # }
     data_loader = {
-        x: data.DataLoader(dataset[x], batch_size=cfg['batch_size'],
+        x: data.DataLoader(data_set[x], batch_size=cfg['batch_size'],
                            num_workers=4, shuffle=True, pin_memory=False)
         for x in ['train', 'val']
     }
-
     ######################################
 
-    model=torchvision.models.detection.maskrcnn_resnet50_fpn()
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn()
     model.cuda()
-    model=nn.DataParallel(model)
+    model = nn.DataParallel(model)
 
     criterion = {
         'crossentropyloss': nn.CrossEntropyLoss(),
@@ -143,6 +149,6 @@ if __name__ == '__main__':
                            lr=cfg['lr'], weight_decay=cfg['weight_decay'])
     scheduler = optim.lr_scheduler.MultiStepLR(
         optimizer, milestones=cfg['milestones'], gamma=cfg['gamma'])
-    
+
     best_model_wts = train_guipang(model, criterion, optimizer, scheduler, cfg)
     torch.save(best_model_wts, the_ckpt_root+'best.pkl')
