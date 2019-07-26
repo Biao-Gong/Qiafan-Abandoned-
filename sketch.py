fd03e7c6f98487162cf654e33f722796db015f68
Gongggg <github@gongbiao.cc>
2019-07-27 00:16:59 +0800

    iou

--- sketch.py
+++ sketch.py
@@ -1,23 +1,20 @@
-import os
-from data import guipang
-import torch.utils.data as data
-import copy
-import os
-import sys
-import json
+# import os
+# import torch.utils.data as data
+# import copy
+# import os
+# import sys
+# import json
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import torchvision
+# import torch.nn as nn
+# import torch.optim as optim
+# import torch.utils.data as data
+# import torchvision
 # from config import get_train_config
-from data import guipang
-from data import qiafan
 # from models.MVHNet import *
 # from utils import append_feature, calculate_map
-import time
-import shutil
-from tensorboardX import SummaryWriter
+# import time
+# import shutil
+# from tensorboardX import SummaryWriter
 import pdb
 dataset_guipang = '/repository/gong/qiafan/guipangdata/'
 # config
@@ -37,6 +34,151 @@ cfg = {
 }
 
 
+x=torch.tensor([1.0,2.0,0])
+y=torch.tensor([[1.0,1.0,1.0,1.0],[2.0,2.0,5.0,5.0],[2.0,2.0,3.0,3.0]])
+z=torch.tensor([[3.0,3.0,6.0,6.0]])
+
+
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes
+
+
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+
+    #Intersection area
+    # Intersection area 这里没有对inter_area为负的情况进行判断，后面计算出来的IOU就可能是负的
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1, min=0)
+
+    #Union Area
+    b1_area = (b1_x2 - b1_x1)*(b1_y2 - b1_y1)
+    b2_area = (b2_x2 - b2_x1)*(b2_y2 - b2_y1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
+
+
+# def bb_intersection_over_union(boxA, boxB):
+# 	# determine the (x, y)-coordinates of the intersection rectangle
+# 	xA = max(boxA[0], boxB[0])
+# 	yA = max(boxA[1], boxB[1])
+# 	xB = min(boxA[2], boxB[2])
+# 	yB = min(boxA[3], boxB[3])
+ 
+# 	# compute the area of intersection rectangle
+# 	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+# 	# compute the area of both the prediction and ground-truth
+# 	# rectangles
+# 	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+# 	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+# 	# compute the intersection over union by taking the intersection
+# 	# area and dividing it by the sum of prediction + ground-truth
+# 	# areas - the interesection area
+# 	iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+# 	# return the intersection over union value
+# 	return iou
+
+
+# def bb_intersection_over_union(boxA, boxB):
+#     boxA = [int(x) for x in boxA]
+#     boxB = [int(x) for x in boxB]
+
+#     xA = max(boxA[0], boxB[0])
+#     yA = max(boxA[1], boxB[1])
+#     xB = min(boxA[2], boxB[2])
+#     yB = min(boxA[3], boxB[3])
+
+#     interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+#     boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+#     boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    
+#     iou = interArea / float(boxAArea + boxBArea - interArea)
+
+#     return iou
+
+
+# def IOU(Reframe,GTframe):
+#     """
+#     自定义函数，计算两矩形 IOU，传入为均为矩形对角线，（x,y）  坐标。
+#     """
+#     x1 = Reframe[:,0]
+#     y1 = Reframe[:,1]
+#     width1 = Reframe[:,2]-Reframe[:,0]
+#     height1 = Reframe[:,3]-Reframe[:,1]
+
+#     x2 = GTframe[0]
+#     y2 = GTframe[1]
+#     width2 = GTframe[2]-GTframe[0]
+#     height2 = GTframe[3]-GTframe[1]
+
+#     endx = max(x1+width1,x2+width2)
+#     startx = min(x1,x2)
+#     width = width1+width2-(endx-startx)
+
+#     endy = max(y1+height1,y2+height2)
+#     starty = min(y1,y2)
+#     height = height1+height2-(endy-starty)
+
+#     if width <=0 or height <= 0:
+#         ratio = 0 # 重叠率为 0 
+#     else:
+#         Area = width*height # 两矩形相交面积
+#         Area1 = width1*height1
+#         Area2 = width2*height2
+#         ratio = Area*1./(Area1+Area2-Area)
+#     # return IOU
+#     return ratio,Reframe,GTframe
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    """
+    Returns the IoU of two bounding boxes
+    """
+    if not x1y1x2y2:
+        # Transform from center and width to exact coordinates
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+    else:
+        # Get the coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+    # get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    # Intersection area
+    inter_area =    torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
+                    torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    # Union Area
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return iou
+
+print(bbox_iou(y,z))
+
+
+
 # print(os.path.join(dataset_guipang,'train')
 # for filename in os.listdir(os.path.join(dataset_guipang,'train')):
 #     if os.path.splitext(filename)[1]=='.jpg':
@@ -51,9 +193,9 @@ cfg = {
 
 # elif os.path.splitext(filename)[1]=='.xml':
 #     annotations.append(filename)
-data_set = {
-    x: guipang(cfg=cfg['dataset_guipang'], part=x) for x in ['train', 'val']
-}
+# data_set = {
+#     x: guipang(cfg=cfg['dataset_guipang'], part=x) for x in ['train', 'val']
+# }
 # data_set = {
 #     x: dataset(cfg=cfg['dataset_qiafan'], part=x) for x in ['train', 'val']
 # }
@@ -66,7 +208,7 @@ data_set = {
 # print(a)
 # pdb.set_trace()
 
-os.environ['CUDA_VISIBLE_DEVICES'] = cfg['cuda_devices']
+# os.environ['CUDA_VISIBLE_DEVICES'] = cfg['cuda_devices']
 
 
 # model = torchvision.models.detection.maskrcnn_resnet50_fpn()
@@ -75,25 +217,29 @@ os.environ['CUDA_VISIBLE_DEVICES'] = cfg['cuda_devices']
 # model.train()
 
 
-phrase = 'train'
-for i in range(int(data_set[phrase].__len__()/cfg['batch_size'])):
-    images = []
-    targets = []
-    for ii in range(cfg['batch_size']):
-        data_set_return = data_set[phrase].__getitem__(i*cfg['batch_size']+ii)
-        images.append(data_set_return[0].cuda())
-        targets.append({
-            'boxes': torch.cuda.FloatTensor([[int(data_set_return[1]['annotation']['object']['bndbox']['xmin']),
-                                              int(data_set_return[1]['annotation']['object']['bndbox']['ymin']),
-                                              int(data_set_return[1]['annotation']['object']['bndbox']['xmax']),
-                                              int(data_set_return[1]['annotation']['object']['bndbox']['ymax'])]]),
-            'labels': torch.cuda.LongTensor([1])
-        })
-        # print(ii)
-        print(data_set_return[0].size()[1])
-        pdb.set_trace()
-    print(images)
-    print(targets)
+# phrase = 'train'
+# for i in range(int(data_set[phrase].__len__()/cfg['batch_size'])):
+#     images = []
+#     targets = []
+#     for ii in range(cfg['batch_size']):
+#         data_set_return = data_set[phrase].__getitem__(i*cfg['batch_size']+ii)
+#         images.append(data_set_return[0].cuda())
+#         targets.append({
+#             'boxes': torch.cuda.FloatTensor([[int(data_set_return[1]['annotation']['object']['bndbox']['xmin']),
+#                                               int(data_set_return[1]['annotation']['object']['bndbox']['ymin']),
+#                                               int(data_set_return[1]['annotation']['object']['bndbox']['xmax']),
+#                                               int(data_set_return[1]['annotation']['object']['bndbox']['ymax'])]]),
+#             'labels': torch.cuda.LongTensor([1])
+#         })
+#         # print(ii)
+#         print(data_set_return[0].size()[1])
+#         pdb.set_trace()
+#     print(images)
+#     print(targets)
+
+
+
+
     # print(data_set[phrase].__getitem__(i))
     # print(cfg['batch_size'])
     # print(i)
@@ -119,7 +265,7 @@ for i in range(int(data_set[phrase].__len__()/cfg['batch_size'])):
     # labels = torch.cuda.LongTensor(
     #     list(map(int, annotations['annotation']['object']['name'])))
 
-    with torch.set_grad_enabled(phrase == 'train'):
-        outputs = model(images,boxes=boxes,labels=labels)
-        print(outputs)
-        pdb.set_trace()
+    # with torch.set_grad_enabled(phrase == 'train'):
+    #     outputs = model(images,boxes=boxes,labels=labels)
+    #     print(outputs)
+    #     pdb.set_trace()
--- train_qiafan.py
+++ train_qiafan.py
@@ -16,11 +16,81 @@ import shutil
 from tensorboardX import SummaryWriter
 
 
-def mapcal(epoch_map,outputs,targets,bar_scor,bar_iou):
+
+def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    
+    intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1, 2))         # Will be zzero if both are 0
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    
+    return thresholded  # Or thresholded.mean() if you are interested in average across the batch
+    
+
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    if not x1y1x2y2:
+        # Transform from center and width to exact coordinates
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+    else:
+        # Get the coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+    # get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    # Intersection area
+    inter_area =    torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
+                    torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    # Union Area
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return iou
+
+
+
+def mapcal_guipang(epoch_map,outputs,targets,bar_scor,bar_iou):
 # [{'boxes': tensor([], device='cuda:0', size=(0, 4)), 'labels': tensor([], device='cuda:0', dtype=torch.int64), 'scores': tensor([], device='cuda:0'), 'masks': tensor([], device='cuda:0', size=(0, 1, 4096, 5500))}, 
 # {'boxes': tensor([], device='cuda:0', size=(0, 4)), 'labels': tensor([], device='cuda:0', dtype=torch.int64), 'scores': tensor([], device='cuda:0'), 'masks': tensor([], device='cuda:0', size=(0, 1, 4096, 5500
 # {'boxes': tensor([], device='cuda:0', size=(0, 4)), 'labels': tensor([], device='cuda:0', dtype=torch.int64), 'scores': tensor([], device='cuda:0'), 'masks': tensor([], device='cuda:0', size=(0, 1, 4096, 5500))}, {'boxes': tensor([], device='cuda:0', size=(0, 4)), 'labels': tensor([], device='cuda:0', dtype=torch.int64), 'scores': tensor([], device='cuda:0'), 'masks': tensor([], device='cuda:0', size=(0, 1, 4096, 5500))}, 
 # {'boxes': tensor([], device='cuda:0', size=(0, 4)), 'labels': tensor([], device='cuda:0', dtype=torch.int64), 'scores': tensor([], device='cuda:0'), 'masks': tensor([], device='cuda:0', size=(0, 1, 4096, 5500))}]
+    for output,target in outputs,targets:
+        boxes=output['boxes']
+        scores=output['scores']
+        boxes_gt=target['boxes']
+
+        sorted_scores=torch.sort(scores)[0]
+        sorted_boxes=boxes[torch.sort(scores)[1]]
+
+        sorted_bar_boxes=sorted_boxes[torch.gt(sorted_scores,bar_scor)]
+
+
+
+
+
+
+
+
+
+
+
+
+    
     return 0.0
 
 
@@ -30,6 +100,7 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
     best_model_wts = copy.deepcopy(model.state_dict())
 
     for epoch in range(1, cfg['max_epoch']):
+        print('epoch: ',epoch)
         for phrase in ['train', 'val']:
             nowepochiter = (epoch-1)*int(data_set[phrase].__len__()/cfg['batch_size'])
             if phrase == 'train':
@@ -42,7 +113,7 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
             # predic, gt = None, None
             epoch_map=0.0
             ##################################################
-
+            #             
             for i in range(int(data_set[phrase].__len__()/cfg['batch_size'])):
 
                 optimizer.zero_grad()
@@ -53,6 +124,7 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
                     data_set_return = data_set[phrase].__getitem__(
                         i*cfg['batch_size']+ii)
                     images.append(data_set_return[0].cuda())
+                    
                     targets.append({
                         'boxes': torch.cuda.FloatTensor([[int(data_set_return[1]['annotation']['object']['bndbox']['xmin']),
                                                           int(
@@ -65,8 +137,8 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
                     })
 
                 with torch.set_grad_enabled(phrase == 'train'):
-                    outputs = model(images, targets)
                     if phrase == 'train':
+                        outputs = model(images, targets)
                         loss = outputs['loss_classifier'] + \
                             outputs['loss_box_reg'] + \
                             outputs['loss_objectness'] + \
@@ -75,7 +147,8 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
                         optimizer.step()
                         writer.add_scalar('guipangtrain/loss',loss.item(), i+nowepochiter)                         
                     else:
-                        epoch_map=mapcal(epoch_map,outputs,targets,bar_scor,bar_iou)
+                        outputs = model(images)
+                        epoch_map=mapcal_guipang(epoch_map,outputs,targets,cfg['bar_scor'],cfg['bar_iou'])
                         
 
             if phrase == 'val':
@@ -86,6 +159,7 @@ def train_guipang(model, criterion, optimizer, scheduler, cfg):
                 if epoch % cfg['checkpoint'] == 0:
                     torch.save(copy.deepcopy(model.module.state_dict()),
                                the_ckpt_root+'{}.pkl'.format(epoch))
+
                 
     return best_model_wts
 
@@ -98,11 +172,13 @@ if __name__ == '__main__':
         'lr': 0.001,
         'momentum': 0.9,
         'weight_decay': 0.0005,
-        'batch_size': 5,
+        'batch_size': 1,
         'max_epoch': 100,
         'checkpoint': 20,
         'milestones': [30, 50],
         'gamma': 0.1,
+        'bar_scor':0.7,
+        'bar_iou':0.5,
         'dataset_guipang': '/repository/gong/qiafan/guipangdata/',
         'dataset_qiafan': '/repository/gong/qiafan/dataset/',
         'cuda_devices': '6',
@@ -128,6 +204,8 @@ if __name__ == '__main__':
     #                        num_workers=4, shuffle=True, pin_memory=False)
     #     for x in ['train', 'val']
     # }
+    assert cfg['batch_size'] <= data_set['train'].__len__()
+    assert cfg['batch_size'] <= data_set['val'].__len__()    
     ######################################
 
     model = torchvision.models.detection.maskrcnn_resnet50_fpn()
